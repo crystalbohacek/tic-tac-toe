0a1f52a18a869a2282dd1e4f1e6ad8c8a5b83749
Crystal <hello@crystalbohacek.com>
2017-06-27 11:26:52 -0400

    added computer player

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,22 +1,49 @@
 var gameBoardEl = document.getElementById("gameBoard");
-var idArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var availableBlocks = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 
 function hasClass( target, className ) {
     return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
 }
 
+function chooseRandomIndex(){
+	return Math.floor(Math.random() * availableBlocks.length);
+}
+
+function computerPlay(){
+	var randomId = chooseRandomIndex();
+	var randomField = availableBlocks[randomId];
+
+	availableBlocks.splice(randomId, 1);	
+
+	setTimeout(function(){
+		document.getElementById(randomField).innerHTML = "<i class='ion-android-radio-button-off'></i>";
+	}, 300);
+}
+
 gameBoardEl.onclick = function(ev){
 	ev.stopPropagation();
-	if (hasClass(ev.target, 'block')){
+	if (hasClass(ev.target, 'block') && availableBlocks.indexOf(ev.target.id) > -1){
 
-		if (idArray.indexOf(ev.target.id) !== -1){
-			idArray.splice(idArray.indexOf(ev.target.id), 1);
+		if (availableBlocks.indexOf(ev.target.id) !== -1){
+			availableBlocks.splice(availableBlocks.indexOf(ev.target.id), 1);
+
 		}
 
 		ev.target.innerHTML = "<i class='ion-close-round'></i>";
+
+		
+		if (availableBlocks.length){
+			computerPlay();
+		}
+		else{
+			setTimeout(function(){
+				alert('Game over!');
+			}, 300)
+		}
+	
 	}
 	return false;
-
 } 
 
-
+//winner logic
