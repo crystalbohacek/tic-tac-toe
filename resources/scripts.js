0658cbdd2f52a53a4f6645c3e95544128fc7532e
Crystal <hello@crystalbohacek.com>
2017-06-27 00:05:21 -0400

    added svg icons and stopped double clicking

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,8 +1,22 @@
 var gameBoardEl = document.getElementById("gameBoard");
+var idArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function hasClass( target, className ) {
+    return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
+}
 
 gameBoardEl.onclick = function(ev){
+	ev.stopPropagation();
+	if (hasClass(ev.target, 'block')){
 
-	ev.target.innerHTML = "<span> X </span>";
+		if (idArray.indexOf(ev.target.id) !== -1){
+			idArray.splice(idArray.indexOf(ev.target.id), 1);
+		}
+
+		ev.target.innerHTML = "<i class='ion-close-round'></i>";
+	}
+	return false;
+
 } 
 
 
