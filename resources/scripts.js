586ca51439d13e83b70b0d3983f30539089e2629
Crystal <hello@crystalbohacek.com>
2017-07-10 19:32:00 -0400

    added classes

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,6 +1,20 @@
 var gameBoardEl = document.getElementById("gameBoard");
 var availableBlocks = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var playerClass = "xClass";
+var computerClass = "oClass";
 
+var winningLines = 
+[
+	["1", "2", "3"],
+	["4", "5", "6"],
+	["7", "8", "9"],
+	["1", "5", "9"],
+	["3", "5", "7"],
+	["1", "4", "7"],
+	["1", "4", "7"],
+	["2", "5", "8"],
+	["3", "6", "9"],
+]
 
 function hasClass( target, className ) {
     return new RegExp('(\\s|^)' + className + '(\\s|$)').test(target.className);
@@ -14,17 +28,16 @@ function computerPlay(){
 	var randomId = chooseRandomIndex();
 	var randomField = availableBlocks[randomId];
 
-	availableBlocks.splice(randomId, 1);	
-
+	availableBlocks.splice(randomId, 1);		
 	setTimeout(function(){
 		document.getElementById(randomField).innerHTML = "<i class='ion-android-radio-button-off'></i>";
 	}, 300);
+	document.getElementById(randomField + "").classList.add(computerClass);
 }
 
 gameBoardEl.onclick = function(ev){
 	ev.stopPropagation();
-	if (hasClass(ev.target, 'block') && availableBlocks.indexOf(ev.target.id) > -1){
-
+	if (hasClass(ev.target, 'block') && !(hasClass(ev.target, 'xClass') || hasClass(ev.target, 'oClass'))){
 		if (availableBlocks.indexOf(ev.target.id) !== -1){
 			availableBlocks.splice(availableBlocks.indexOf(ev.target.id), 1);
 
@@ -32,7 +45,8 @@ gameBoardEl.onclick = function(ev){
 
 		ev.target.innerHTML = "<i class='ion-close-round'></i>";
 
-		
+		ev.target.classList.add(playerClass);
+
 		if (availableBlocks.length){
 			computerPlay();
 		}
